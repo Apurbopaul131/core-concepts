e737280cef49eea60d9872e90773c53a6114d6a6
Apurbopaul131 <apurbopaul131@gmail.com>
2021-03-03 01:20:04 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+function london(){
+  return (<div>
+    <h1>My name: Apurbo paul </h1>
+    <p>My father name is Shanti rangan paul</p>
+  </div>);
+}
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>My name is Apurbo paul</p>
+        <london></london>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
